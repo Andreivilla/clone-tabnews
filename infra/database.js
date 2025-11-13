1fea6a5f674ed7f37321e634cdc9ae75936eed62
Andreivilla <andreivilla63@gmail.com>
2025-11-13 17:25:41 -0300

    adds `api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,24 +1,8 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
-  });
-  console.log('Credenciais do Postgres: ', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  });
-
+  let client = await getNewClient();
   try {
-    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -28,6 +12,31 @@ async function query(queryObject) {
     await client.end();
   }
 }
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === 'production' ? true : false;
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };

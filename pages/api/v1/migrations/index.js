a08bb1a6b69a46b8b699dc51acb3478c9581bedd
Andreivilla <andreivilla63@gmail.com>
2025-11-19 18:40:25 -0300

    fiz endpoint `/migrations` method connections

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,13 @@ import { join } from 'node:path';
 import database from 'infra/database';
 
 export default async function migrations(request, response) {
+  const allowedMethods = ['GET', 'POST'];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+
   const dbClient = await database.getNewClient();
 
   const defaultMigrationsOptions = {
@@ -16,7 +23,6 @@ export default async function migrations(request, response) {
 
   if (request.method === 'GET') {
     const pendingMigrations = await migrarionsRunner(defaultMigrationsOptions);
-    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -26,13 +32,10 @@ export default async function migrations(request, response) {
       dryRun: false,
     });
 
-    await dbClient.end();
-
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
 
     return response.status(200).json(migratedMigrations);
   }
-  return response.status(405).end();
 }
